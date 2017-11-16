eb795dc2c33db4a5923c7a1f041cd5aad0c5c472
k2nxf <morphew@gmail.com>
2017-11-16 10:35:58 -0800

    Added some define statements to utilities.h for easier debugging.

--- code/clean/utilities.h
+++ code/clean/utilities.h
@@ -14,6 +14,8 @@
 #define AMPLITUDE OCR0B                 // VCA amplitude control output
 #define DAC_CS 7                        // use pin 7 on PORTD for DAC CS
 #define MEM_CS 4                        // use pin 4 on PORTD for MEM_CS
+#define ADC_INIT 0xE3                   // init value for ADSRA register
+#define ADMUX_INIT 0x40                 // init values for ADMUX (right adjust, internal VCC, gain x200
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -24,8 +26,8 @@
 // -------- INITILIZATION ---------- //
 void adc_init(void) {
     // setup ADC
-    ADMUX = 0x40;                         // right adjust, ADC0, internal VCC, gain x200
-    ADCSRA = 0xe3;                        // enable ADC, clk/64, disable auto trigger
+    ADMUX = ADMUX_INIT;                         // right adjust, ADC0, internal VCC, gain x200
+    ADCSRA = ADC_INIT;                        // enable ADC, clk/64, disable auto trigger
     ADCSRB =0x01;                         // analog comparator mode
     DIDR0 = 0x3F;                         // turn off digital inputs for all ADC channels
 }
@@ -50,15 +52,15 @@ void spi_init(void) {
     DDRD |= (1<<DAC_CS) | (1<<MEM_CS);   // enable output on DAC_CS and MEM_CS
     PORTD |= (1<<DAC_CS);                // pull DAC_CS high
     PORTD |= (1<<MEM_CS);                // pull MEM_CS high
-    setup_spi(0x00,0,0,0x04);            // setup SPI interface (mode 0, MSB first, no interrupt, clk/2)
+    setup_spi(0x00,0,0,0x00);            // setup SPI interface (mode 0, MSB first, no interrupt, clk/2)
 }
 
 // -------- UTILITIES ---------- //
 uint16_t adc_read(uint8_t channel) {
-    ADMUX = 0x40;                         // clear last ADC channel
+    ADMUX = ADMUX_INIT;                         // clear last ADC channel
     ADMUX |= channel;                     // select new ADC
     ADCSRA |= (1<<ADSC);                  // start conversion
-    while(ADCSRA == 0xe3);                // wait for conversion
+    while(ADCSRA == ADC_INIT);                // wait for conversion
     uint8_t temp_L;
     uint16_t temp_H;
     temp_L = ADCL;
@@ -67,7 +69,7 @@ uint16_t adc_read(uint8_t channel) {
 }
 
 void dac_write(uint16_t value) {
-    uint8_t config_bits = 0x05;           // pull STDN and BUF high
+    uint8_t config_bits = 0x07;           // pull STDN and BUF high
     uint8_t temp_H = ((value>>8) & 0x0F) | (config_bits<<4); // set first 4 config bits to 0
     uint8_t temp_L = (value & 0x00FF);    // clear out first 8 bits for truncation
     PORTD &= ~(1<<DAC_CS);                // set DAC_CS low to enable
