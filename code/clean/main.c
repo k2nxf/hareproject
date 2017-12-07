d6bfbfc7b94e9c2ab2d0289662103dc199a76c16
k2nxf <morphew@gmail.com>
2017-12-06 16:32:24 -0800

    Latest clean code update. Photos of prototypes also uploaded.

--- code/clean/main.c
+++ code/clean/main.c
@@ -3,12 +3,11 @@
  *
  *  Created on: Nov 13, 2017
  *      Author: abram
- *      Revision: 20171115
+ *      Revision: 20171205
  *      References:
  */
 
 #include "utilities.h"
-#define MAX_ADC 4                                     // max number of ADC channels
 
 // ------------- MAIN PROGRAM ------------ //
 int main(void) {
@@ -17,18 +16,21 @@ int main(void) {
     pwm_init();
     spi_init();
 
+    AMPLITUDE = 255;
+    //amplitude_write(255);
     // setup program variables
     uint8_t n = 0;                                    // ADC counter
-    uint16_t audio = 0;                               // audio sample from ADC
+    //uint16_t audio = 0;                               // audio sample from ADC
 
     // main processing loop: create effect here
     while(1) {
-        n = (n>(MAX_ADC - 1)) ? 0 : n;                // if n>3, reset n to 0
+        //if (adc_flag == 1) { adc_flag = 0; }          // reset flag if conversion is complete
+        n = (n>(MAX_ADC - 1)) ? 0 : n;
 
         switch(n) {                                   // grab the ADC values
             case 0:                                   // read value on ADC0
-                audio = adc_read(n);
-                dac_write(audio);
+                audio = adc_read(n)/4;
+                dac_write(4*audio);
                 break;
 
             case 1:
@@ -42,8 +44,10 @@ int main(void) {
             case 3:
                 amplitude_write((adc_read(n))/4);     // read value on ADC3
                 break;
+
             }
         n++;
+
        // _delay_us(10);                                 // short delay before next pass
     }
 }
--- code/clean/utilities.h
+++ code/clean/utilities.h
@@ -3,19 +3,20 @@
  *
  *  Created on: Nov 13, 2017
  *      Author: abram
- *      Revision: 20171115
+ *      Revision: 20171205
  *      References:
  */
 
 #ifndef UTILITIES_H_
 #define UTILITIES_H_
-#define CUTOFF OCR0A                    // VCF cutoff control output
-#define RESONANCE OCR2B                 // VCF resonance control output
-#define AMPLITUDE OCR0B                 // VCA amplitude control output
 #define DAC_CS 7                        // use pin 7 on PORTD for DAC CS
 #define MEM_CS 4                        // use pin 4 on PORTD for MEM_CS
-#define ADC_INIT 0xE3                   // init value for ADSRA register
+#define ADC_START 0xE6                  // init value for ADSRA register
 #define ADMUX_INIT 0x40                 // init values for ADMUX (right adjust, internal VCC, gain x200
+#define CUTOFF OCR0B                    // VCF cutoff control output
+#define RESONANCE OCR0A                 // VCF resonance control output
+#define AMPLITUDE OCR2B                 // VCA amplitude control output
+#define MAX_ADC 4                       // max number of ADC channels
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -23,13 +24,24 @@
 #include <spi/spi.h>
 #include <spi/spi.c>
 
+// -------- GLOBAL VARIABLES ------- //
+uint8_t n = 0;                           // ADC channel counter
+uint8_t adc_flag = 0;                    // ADC complete flag
+uint16_t audio = 0;                      // sampled value from ADC0
+uint16_t digital_pot1 = 0;               // sampled value from ADC1
+uint16_t digital_pot2 = 0;               // sampled value from ADC2
+uint16_t digital_pot3 = 255;               // sampled value from ADC3
+
+
 // -------- INITILIZATION ---------- //
 void adc_init(void) {
     // setup ADC
-    ADMUX = ADMUX_INIT;                         // right adjust, ADC0, internal VCC, gain x200
-    ADCSRA = ADC_INIT;                        // enable ADC, clk/64, disable auto trigger
+    ADMUX = ADMUX_INIT;                   // right adjust, ADC0, internal VCC, gain x200
     ADCSRB =0x01;                         // analog comparator mode
     DIDR0 = 0x3F;                         // turn off digital inputs for all ADC channels
+    ADCSRA = ADC_START;                   // enable ADC, clk/128, disable auto trigger
+    SMCR = 0x01;                          // enable ADC noise reduction mode
+
 }
 
 void pwm_init(void) {
@@ -44,32 +56,31 @@ void pwm_init(void) {
     // TIMER 2
     TCCR2A = 0xA3;                        // same as Timer 0 for Timer 2
     TCCR2B = 0x01;
-
-    SREG = 0x80;                          // enable global interrupts
+    sei();                                // enable global interrupts
 }
 
 void spi_init(void) {
-    DDRD |= (1<<DAC_CS) | (1<<MEM_CS);   // enable output on DAC_CS and MEM_CS
-    PORTD |= (1<<DAC_CS);                // pull DAC_CS high
-    PORTD |= (1<<MEM_CS);                // pull MEM_CS high
-    setup_spi(0x00,0,0,0x00);            // setup SPI interface (mode 0, MSB first, no interrupt, clk/2)
+    DDRD |= (1<<DAC_CS) | (1<<MEM_CS);    // enable output on DAC_CS and MEM_CS
+    PORTD |= (1<<DAC_CS);                 // pull DAC_CS high
+    PORTD |= (1<<MEM_CS);                 // pull MEM_CS high
+    setup_spi(0x00,0,0,0x04);             // setup SPI interface (mode 0, MSB first, no interrupt, clk/2)
 }
 
 // -------- UTILITIES ---------- //
 uint16_t adc_read(uint8_t channel) {
     ADMUX = ADMUX_INIT;                         // clear last ADC channel
-    ADMUX |= channel;                     // select new ADC
-    ADCSRA |= (1<<ADSC);                  // start conversion
-    while(ADCSRA == ADC_INIT);                // wait for conversion
+    ADMUX |= channel;                           // select new ADC
+    ADCSRA |= (1<<ADSC);                        // start conversion
+    while(ADCSRA == ADC_START);                 // wait for conversion
     uint8_t temp_L;
     uint16_t temp_H;
     temp_L = ADCL;
     temp_H = (ADCH<<8) & 0xFF00;
-    return (temp_H | temp_L);             // combine high and low bit from ADC
+    return (temp_L | temp_H);                   // combine high and low bit from ADC
 }
 
 void dac_write(uint16_t value) {
-    uint8_t config_bits = 0x07;           // pull STDN and BUF high
+    uint8_t config_bits = 0x05;           // pull SHDN and BUF high
     uint8_t temp_H = ((value>>8) & 0x0F) | (config_bits<<4); // set first 4 config bits to 0
     uint8_t temp_L = (value & 0x00FF);    // clear out first 8 bits for truncation
     PORTD &= ~(1<<DAC_CS);                // set DAC_CS low to enable
@@ -100,4 +111,18 @@ void amplitude_write(uint8_t value) {     // VCA amplitude control function
     AMPLITUDE = value;
 }
 
+// ------ INTERRUPT SERVICE ROUTINES ----- //
+/*
+ISR(ADC_vect){
+    //n = (n>(MAX_ADC - 1)) ? 0 : n;        // if n>3, reset n to 0
+    adc_flag = 1;
+    uint8_t temp_L;
+    uint16_t temp_H;
+    temp_L = ADCL;
+    temp_H = (ADCH<<8) & 0xFF00;
+    audio = 0;
+    dac_write((temp_L | temp_H));
+
+}
+*/
 #endif /* UTILITIES_H_ */
